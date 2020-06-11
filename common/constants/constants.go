96317b6c1a4befe95d62a15ec33950397d6a87db
Yihen <liuyihen@gmail.com>
2020-06-11 15:42:28 +0800

    use Rinkeby contract address of DataProof

--- common/constants/constants.go
+++ common/constants/constants.go
@@ -4,6 +4,6 @@ package constants
 const (
 	DEFAULT_WORKSPACE = "~/.nilfs"
 	DEFAULT_IPFS_DIR  = "./.ipfs"
-	CONTRACT_ADDR     = "pdp-contract-address"
+	CONTRACT_ADDR     = "0x4befef1f86b12fa7d62da486acdddb90656801ead596771bbd0137877dbcd1a4" //test-net of Rinkeby
 	ACCOUNT_KEY       = ""
 )
