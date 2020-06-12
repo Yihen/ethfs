23decc85c709de5c41da78dc57d80d785751fe66
Yihen <liuyihen@gmail.com>
2020-06-12 17:57:56 +0800

    add config.json for default start

--- common/constants/constants.go
+++ common/constants/constants.go
@@ -5,5 +5,25 @@ const (
 	DEFAULT_ETH_WORKSPACE = "~/.ethereum/"
 	DEFAULT_IPFS_DIR      = "~/.ipfs/"
 	CONTRACT_ADDR         = "0x4befef1f86b12fa7d62da486acdddb90656801ead596771bbd0137877dbcd1a4" //test-net of Rinkeby
-	ACCOUNT_KEY           = ""
+	ACCOUNT_KEY           = `{
+									"address": "1a9ec3b0b807464e6d3398a59d6b0a369bf422fa",
+									"crypto": {
+									"cipher": "aes-128-ctr",
+									"ciphertext": "a471054846fb03e3e271339204420806334d1f09d6da40605a1a152e0d8e35f3",
+									"cipherparams": {
+									"iv": "44c5095dc698392c55a65aae46e0b5d9"
+									},
+									"kdf": "scrypt",
+									"kdfparams": {
+									"dklen": 32,
+									"n": 262144,
+									"p": 1,
+									"r": 8,
+									"salt": "e0a5fbaecaa3e75e20bccf61ee175141f3597d3b1bae6a28fe09f3507e63545e"
+									},
+									"mac": "cb3f62975cf6e7dfb454c2973bdd4a59f87262956d5534cdc87fb35703364043"
+									},
+									"id": "e08301fb-a263-4643-9c2b-d28959f66d6a",
+									"version": 3
+							}`
 )
